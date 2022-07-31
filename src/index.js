de06fb49e5e3092013256348714c18cf730cf091
Mauricio Junior <mauriciojunior@Mauricios-MacBook-Air.local>
2022-07-31 11:48:07 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import {Book} from "./application/Book";
+import {Chapter} from "./application/Chapter";
+import {Character} from "./application/Character";
+import {Movie} from "./application/Movie";
+import {Quote} from "./application/Quote";
+
+module.exports = {
+    Book,
+    Chapter,
+    Character,
+    Movie,
+    Quote
+}
